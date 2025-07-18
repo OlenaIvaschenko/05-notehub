cb1e177f3a4292730a096ec002f5f240c758aa92
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-07-18 23:22:11 +0300

    task-start-get

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,25 +4,38 @@ import { fetchNotes} from "../servises/noteService";
 import css from "./App.module.css";
 import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { useState } from "react";
-import SearchBox from "./SearchBox/SearchBox";
+import SearchBox from "../SearchBox/SearchBox";
+import { useDebouncedCallback } from "use-debounce";
+import ReactPaginate from 'react-paginate';
+import Pagination from "../Pagination/Pagination";
+
+
 
 export default function App() {
   const [query, setQuery] = useState("");
    const [page, setPage] = useState(1);
+   const updateQuery = useDebouncedCallback((query)=>{
+    setQuery(query);
+    setPage(1);
+  }, 1000);
+
 
    const { data, isLoading, isError, isSuccess } = useQuery({
     queryKey: ["notes", query, page],
     queryFn: () => fetchNotes(query, page),
     // enabled: query !== "",
-    // placeholderData: keepPreviousData,
+    placeholderData: keepPreviousData,
   });
 
   return (
    
      <div className={css.app}>
 	<header className={css.toolbar}>
-		<SearchBox/>
-		{/* Пагінація */}
+		<SearchBox value ={query} onSearch={updateQuery}/>
+		{data&&data.totalPages>1&&<Pagination page={page} total={data.totalPages} onChange={setPage}/>
+	
+
+}
 		<button className={css.button}>Create note +</button>
 
   </header>
