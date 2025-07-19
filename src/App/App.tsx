474939d417dc7e76e7cdd45e2659d9170453aa89
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-07-20 02:01:30 +0300

    modal-form-validation

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,5 @@
 import NoteList from "../NoteList/NoteList";
-import { fetchNotes} from "../servises/noteService";
+import { fetchNotes } from "../servises/noteService";
 
 import css from "./App.module.css";
 import { useQuery, keepPreviousData } from "@tanstack/react-query";
@@ -8,41 +8,50 @@ import SearchBox from "../SearchBox/SearchBox";
 import { useDebouncedCallback } from "use-debounce";
 // import ReactPaginate from 'react-paginate';
 import Pagination from "../Pagination/Pagination";
-
-
+import Modal from "../Modal/Modal";
+import NoteForm from "../NoteForm/NoteForm";
 
 export default function App() {
   const [query, setQuery] = useState("");
-   const [page, setPage] = useState(1);
-   const updateQuery = useDebouncedCallback((query)=>{
+  const [page, setPage] = useState(1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const updateQuery = useDebouncedCallback((query) => {
     setQuery(query);
     setPage(1);
   }, 1000);
 
-
-   const { data } = useQuery({
+  const { data } = useQuery({
     queryKey: ["notes", query, page],
     queryFn: () => fetchNotes(query, page),
     // enabled: query !== "",
     placeholderData: keepPreviousData,
   });
 
+  const openModal = () => setIsModalOpen(true);
+
+  const closeModal = () => setIsModalOpen(false);
+
   return (
-   
-     <div className={css.app}>
-	<header className={css.toolbar}>
-		<SearchBox value ={query} onSearch={updateQuery}/>
-		{data&&data.totalPages>1&&<Pagination page={page} total={data.totalPages} onChange={setPage}/>
-	
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        <SearchBox value={query} onSearch={updateQuery} />
+        {data && data.totalPages > 1 && (
+          <Pagination page={page} total={data.totalPages} onChange={setPage} />
+        )}
+        <button className={css.button} onClick={openModal}>
+          Create note +
+        </button>
 
-}
-		<button className={css.button}>Create note +</button>
-
-  </header>
-  <NoteList notes={data?data.notes:[]}/>
-</div> 
-    
+        {isModalOpen && (
+          <Modal onClose={closeModal}>
+            
+            <NoteForm />
+            
+          </Modal>
+        )}
+      </header>
+      <NoteList notes={data ? data.notes : []} />
+    </div>
   );
 }
-
-
--- src/NoteForm/NoteForm.tsx
+++ src/NoteForm/NoteForm.tsx
@@ -1,35 +1,95 @@
 import css from "./NoteForm.module.css"
+import { Formik, Form, Field, type FormikHelpers, ErrorMessage  } from "formik";
+import { useId } from "react";
+import * as Yup from "yup";
+
+const NoteFormSchema = Yup.object().shape({
+  title: Yup.string()
+    .min(3, "Must be at least 3 characters")
+    .max(50, "Too long")
+    .required("Required field"),
+  content: Yup.string()
+    .max(500, "Too long"),
+  tag: Yup.string()
+    .oneOf(["Todo", "Work", "Personal", "Meeting", "Shopping"])
+    .required("Required field"),
+
+});
+
+
+interface NoteFormValues {
+  title: string;
+  content: string;
+  tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+}
+
+const initialValues: NoteFormValues = {
+  title: "",
+  content: "",
+  tag: "Todo"
+};
 
 export default function NoteForm (){
+   const fieldId = useId();
+
+const handleSubmit = (
+    values: NoteFormValues,
+    actions: FormikHelpers<NoteFormValues>
+  ) => {
+    console.log("Order data:", values);
+    actions.resetForm();
+  };
+
 return (
-    <form className={css.form}>
+
+   <Formik 
+   initialValues={initialValues} 
+   onSubmit={handleSubmit} 
+   validationSchema={NoteFormSchema}>
+      
+
+    <Form className={css.form}>
   <div className={css.formGroup}>
-    <label htmlFor="title">Title</label>
-    <input id="title" type="text" name="title" className={css.input} />
-    {/* <span name="title" className={css.error} /> */}
+    <label htmlFor={`${fieldId}-title`}>Title</label>
+    <Field id={`${fieldId}-title`} type="text" name="title" className={css.input} />
+    
+    <ErrorMessage 
+    component="span" 
+    name="title" 
+    className={css.error} 
+    />
+  
   </div>
 
   <div className={css.formGroup}>
-    <label htmlFor="content">Content</label>
-    <textarea
-      id="content"
+    <label htmlFor={`${fieldId}-content`}>Content</label>
+    <Field 
+    as="textarea"
+      id={`${fieldId}-content`}
       name="content"
       rows={8}
       className={css.textarea}
     />
-    {/* <span name="content" className={css.error} /> */}
+    <ErrorMessage 
+    component="span" 
+     name="content" 
+     className={css.error} />
   </div>
 
   <div className={css.formGroup}>
-    <label htmlFor="tag">Tag</label>
-    <select id="tag" name="tag" className={css.select}>
+    <label htmlFor={`${fieldId}-tag`}>Tag</label>
+    <Field as="select" id={`${fieldId}-tag`} name="tag" className={css.select}>
       <option value="Todo">Todo</option>
       <option value="Work">Work</option>
       <option value="Personal">Personal</option>
       <option value="Meeting">Meeting</option>
       <option value="Shopping">Shopping</option>
-    </select>
-    {/* <span name="tag" className={css.error} /> */}
+    </Field>
+
+    <ErrorMessage 
+    component="span" 
+    name="tag" 
+    className={css.error} />
   </div>
 
   <div className={css.actions}>
@@ -39,13 +99,12 @@ return (
     <button
       type="submit"
       className={css.submitButton}
-    //   disabled=false
+      disabled={false}
     >
       Create note
     </button>
   </div>
-</form>
-
-
+</Form>
+</Formik>
 )
 }
